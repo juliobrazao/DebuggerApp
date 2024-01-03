9785912cd3107e0aff5e8448e16eb096ad3ff4f3
Julio Brazao <jotabrazao@gmail.com>
2024-01-03 09:30:24 -0400

    Initial Commit

---
+++ DebuggerApp/DebuggerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebuggerAppApp.swift
+//  DebuggerApp
+//
+//  Created by Julio Brazao on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DebuggerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
